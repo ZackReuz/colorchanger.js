7ea654b890ff12ebc4caa192330ae6a5e3de80f3
ZackReuz <yasishazz0@gmail.com>
2020-12-10 15:16:40 +0530

    first js project

---
+++ js/script.js
@@ -0,0 +1,58 @@
+document.getElementById('red').addEventListener("click", function(e) {
+
+
+    console.log(e)
+    document.getElementById('container').style.backgroundColor = "red"
+
+})
+
+document.getElementById('blue').addEventListener("click", function(e) {
+
+
+    console.log(e)
+    document.getElementById('container').style.backgroundColor = "blue"
+
+})
+document.getElementById('violet').addEventListener("click", function(e) {
+
+
+    console.log(e)
+    document.getElementById('container').style.backgroundColor = " rgb(162, 27, 162)"
+
+})
+document.getElementById('pink').addEventListener("click", function(e) {
+
+
+    console.log(e)
+    document.getElementById('container').style.backgroundColor = "rgb(231, 81, 131)"
+
+})
+document.getElementById('yellow').addEventListener("click", function(e) {
+
+
+    console.log(e)
+    document.getElementById('container').style.backgroundColor = " yellow"
+
+})
+document.getElementById('cyan').addEventListener("click", function(e) {
+
+
+    console.log(e)
+    document.getElementById('container').style.backgroundColor = "cyan"
+
+})
+
+document.getElementById('magenta').addEventListener("click", function(e) {
+
+
+    console.log(e)
+    document.getElementById('container').style.backgroundColor = "magenta"
+
+})
+document.getElementById('reset').addEventListener("click", function(e) {
+
+
+    console.log(e)
+    document.getElementById('container').style.backgroundColor = "#ffffff"
+
+})
